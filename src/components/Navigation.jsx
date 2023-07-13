ded4c4090ac3adff74590bebd0217cbe6e539abe
Newtayo <akandeabdul@gmail.com>
2023-07-13 20:19:33 +0100

    Add navlinks

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './styles/navigation.css';
+import myImage from './images/tag.JPG';
+const Navigation = () => (
+  <nav className="navbar">
+    <img src={myImage} alt="Tag" className='picture' />
+    <ul className='link'>
+    <NavLink className="lifestyle" to="/">MODELS</NavLink>
+    <NavLink className="lifestyle" to="/lifestyle/">LIFESTYLE</NavLink>
+    <NavLink className="lifestyle" to="/shop/">SHOP</NavLink>
+    <NavLink className="lifestyle" to="/test/">TEST LAPTOP</NavLink>
+    </ul>
+  </nav>
+);
 
-const Navigation = () => {
-  return (
-    <div>Navigation</div>
-  )
-}
-
-export default Navigation
+export default Navigation;
---
+++ src/components/Testlaptop.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Testlaptop = () => {
+  return (
+    <div>Testlaptop</div>
+  )
+}
+
+export default Testlaptop
