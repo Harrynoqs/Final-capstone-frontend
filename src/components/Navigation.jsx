696dfb7787010b3e744875e43e4725f7b8a0a405
Newtayo <akandeabdul@gmail.com>
2023-07-13 20:59:22 +0100

    Add social media icons and footer

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './styles/navigation.css';
 import myImage from './images/tag.JPG';
+import * as Falcons from "react-icons/fa6";
+import * as Tiles from "react-icons/ti";
 const Navigation = () => (
   <nav className="navbar">
     <img src={myImage} alt="Tag" className='picture' />
@@ -11,6 +13,16 @@ const Navigation = () => (
     <NavLink className="lifestyle" to="/shop/">SHOP</NavLink>
     <NavLink className="lifestyle" to="/test/">TEST LAPTOP</NavLink>
     </ul>
+    <div className='footer'>
+    <ul className='icons'>
+    <NavLink to="#"><Falcons.FaTwitter/></NavLink>
+    <NavLink to="#"><Falcons.FaFacebookF/></NavLink>
+    <NavLink to="#"><Tiles.TiSocialGooglePlus/></NavLink>
+    <NavLink to="#"><Falcons.FaVimeoV/></NavLink>
+    <NavLink to="#"><Falcons.FaPinterestP/></NavLink>
+    </ul>
+    <p className='footnote'>&copy; 2023 Omotayo-Harry-Queenta</p>
+    </div>
   </nav>
 );
 
