6d0a43d6c53095213fda15fdf286eaef35a77417
Newtayo <akandeabdul@gmail.com>
2023-07-14 14:15:34 +0100

    Set up redux store to display laptop on mainpage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Bookcard from './Bookcard';
+import AddBook from './AddBook';
+import { getBooks } from '../redux/books/booksSlice';
 
-const Home = () => <h1>Welcome to our website!</h1>;
-export default Home;
+const Home = () => {
+  const { laptoplibrary } = useSelector((state) => state.book);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  },
+  [dispatch, success]);
+
+  if (isLoading) {
+    return (
+      <h3>Loading...</h3>
+    );
+  }
+
+  return (
+    <div>
+      { bookslibrary.map((books) => (
+
+        <Bookcard key={books.item_id} book={books} />
+      ))}
+      <AddBook />
+    </div>
+  );
+};
+
+export default Booklist;
--- src/redux/laptop/laptopSlice.js
+++ src/redux/laptop/laptopSlice.js
@@ -1,1 +1,21 @@
-// import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+
+  laptoplibrary: [{"image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwUCv-mnBgilD_YNXW-6pR8rmYw3oHqAkKkA&usqp=CAU", "model" : "Acer", "name" :"Acer laptop"}
+  ]
+}
+
+const booksSlice = createSlice({
+  name: 'library',
+  initialState,
+
+  reducers: {},
+  
+});
+
+
+
+export default laptopSlice.reducer;
+
