b8c74d9d1e603f94dccb8a9a1a30f7064eb7949d
QueenterJuma <jumahqueenter@gmail.com>
2023-07-19 16:11:06 -0700

    Modified laptopSlice.js

--- src/redux/laptop/laptopSlice.js
+++ src/redux/laptop/laptopSlice.js
@@ -1,35 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// const initialState = {
-
-//   laptoplibrary: [{
-//     image: 'https://www.lenovo.com/medias/lenovo-laptops-thinkbook-16-gen-4-intel-hero.png?context=bWFzdGVyfHJvb3R8MzQ1OTM2fGltYWdlL3BuZ3xoMjEvaGZkLzEzMjU1MTI1OTkxNDU0LnBuZ3xlMGJjMDAyZjIzYzczYmY0YTY3NTlmODcwMDJjZTBhMzg5M2VlMjFlNTNlZWJkZDMyNDA3MTdlNjc3NjhhZWY5', model: 'ThinkBook 16 Gen 4', name: 'Lenovo', id: 1, description: 'HP 15.6 Full HD (1920 x 1080) Laptop, Intel Core i5-1135G7, 8GB RAM, 256GB SSD, Windows 10 Home, Natural',
-//   },
-
-//   {
-//     image: 'https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c07993148.png', model: 'HP Laptop 15-dw3350ne', name: 'Hp', id: 2, description: 'HP 15.6 Full HD (1920 x 1080) Laptop, Intel Core i5-1135G7, 8GB RAM, 256GB SSD, Windows 10 Home, Natural',
-//   },
-//   {
-//     image: 'https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/15-7530/media-gallery/laptop-latitude-15-7530-gray-gallery-4.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=666&qlt=100,1&resMode=sharp2&size=666,402&chrss=full', model: 'Dell Latitude 7530', name: 'Dell', id: 3, description: 'HP 15.6 Full HD (1920 x 1080) Laptop, Intel Core i5-1135G7, 8GB RAM, 256GB SSD, Windows 10 Home, Natural',
-//   },
-//   {
-//     image: 'https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/15-7530/media-gallery/laptop-latitude-15-7530-gray-gallery-4.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=666&qlt=100,1&resMode=sharp2&size=666,402&chrss=full', model: 'Dell Latitude 7530', name: 'Dell', id: 4, description: 'HP 15.6 Full HD (1920 x 1080) Laptop, Intel Core i5-1135G7, 8GB RAM, 256GB SSD, Windows 10 Home, Natural',
-//   },
-//   {
-//     image: 'https://i.dell.com/is/image/DellContent//content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/14-3420/media-gallery/peripherals_laptop_latitude_3420nt_gallery_1.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=555&qlt=100,1&resMode=sharp2&size=555,402&chrss=full', model: 'Latitude 14-Inch 3420', name: 'Dell', id: 5, description: 'HP 15.6 Full HD (1920 x 1080) Laptop, Intel Core i5-1135G7, 8GB RAM, 256GB SSD, Windows 10 Home, Natural',
-//   },
-//   ],
-// };
-
-// const laptopSlice = createSlice({
-//   name: 'library',
-//   initialState,
-
-//   reducers: {},
-
-// });
-export const BASE_URL = 'http://localhost:3000/api/v1/laptops';
+export const BASE_URL = 'http://localhost:3000/api/v1';
 
 export const fetchLaptops = createAsyncThunk(
   'laptops/fetchData',
@@ -44,8 +16,8 @@ export const fetchLaptops = createAsyncThunk(
 );
 
 const initialState = {
-  laptoplibrary: [],
-  laptop: {},
+  laptoplibrary: [], // Declare the laptoplibrary array
+  laptop: {}, // Declare the laptop object
 };
 
 export const laptopSlice = createSlice({
@@ -55,7 +27,7 @@ export const laptopSlice = createSlice({
   extraReducers: {
     [fetchLaptops.fulfilled]: (state, action) => {
       state.laptops = action.payload;
-      laptoplibrary = state.laptops;
+      state.laptoplibrary = state.laptops; // Use state.laptoplibrary instead of laptoplibrary
 
       // Extract the objects of laptops to a single laptop and assign it to laptop in the state
       state.laptop = state.laptops.reduce((acc, laptop) => ({
@@ -73,7 +45,6 @@ export const laptopSlice = createSlice({
       }), {});
     },
   },
-
 });
 
 export default laptopSlice.reducer;
