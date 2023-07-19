fcf402c848a99e49b5e3ae2af4bdbc27e644e43d
QueenterJuma <jumahqueenter@gmail.com>
2023-07-19 12:22:05 -0700

    Add Fetch laptop fuction to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import './App.css';
@@ -6,19 +7,26 @@ import Navigation from './components/Navigation';
 import Lifestyle from './components/Lifestyle';
 import Shop from './components/Shop';
 import Testlaptop from './components/Testlaptop';
+import { fetchLaptops } from './redux/laptop/laptopSlice';
 
-const App = () => (
-  <Router>
-    <Navigation />
-    <div className="container">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lifestyle" element={<Lifestyle />} exact />
-        <Route path="/shop" element={<Shop />} exact />
-        <Route path="/test" element={<Testlaptop />} exact />
-      </Routes>
-    </div>
-  </Router>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchLaptops);
+  }, []);
+  return (
+    <Router>
+      <Navigation />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lifestyle" element={<Lifestyle />} exact />
+          <Route path="/shop" element={<Shop />} exact />
+          <Route path="/test" element={<Testlaptop />} exact />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
